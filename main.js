2b2910036f9e1ae22bf53ab88e207701536979fb
Grace <gracemarsh27@gmail.com>
2021-09-15 22:47:02 +1000

    increased accessibility
    - added ARIA and enabled tabbing + closing modal with esc.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const gridContainer = document.querySelector(".grid-container")
 const overlay = document.querySelector(".overlay")
 const modalContainer = document.querySelector(".modal-content")
 const modalClose = document.querySelector(".modal-close")
+const modal = document.querySelector(".modal")
 
 function displayEmployees(employeeData) {
   employees = employeeData
@@ -19,11 +20,11 @@ function displayEmployees(employeeData) {
     let picture = employee.picture
     // template literals make this so much cleaner!
     employeeHTML += `
-        <div class="card" data-index="${index}">
+        <div class="card pointer" tabindex="0" role="button" data-index="${index}">
           <img class="avatar" src="${picture.large}" />
           <div class="text-container">
             <h2 class="name">${name.first} ${name.last}</h2>
-            <p class="email">${email}</p>
+            <p class="email overflow ellipsis" abbr title="${email}">${email}</p>
             <p class="address">${city}</p>
           </div>
         </div>
@@ -60,16 +61,21 @@ function displayModal(index) {
       <p class="address">${city}</p>
       <hr />
       <p>${phone}</p>
-      <p class="address">${street}, ${state} ${postcode}</p>
+      <p class="address">${street.number} ${
+    street.name
+  }, ${state} ${postcode}</p>
       <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
 `
   overlay.classList.remove("hidden")
-  debugger
 
   // deletes anything inside container and replaces it with new things
   modalContainer.innerHTML = modalHTML
+  modalClose.focus()
 }
+
+// DRY. MAKE THE REPEATED CODE A FUNCTION!
+
 gridContainer.addEventListener("click", (e) => {
   // make sure the click is not on the gridContainer itself!!
   if (e.target !== gridContainer) {
@@ -80,6 +86,33 @@ gridContainer.addEventListener("click", (e) => {
   }
 })
 
-// modalClose.addEventListener("click", () => {
-//   overlay.classList.add("hidden")
-// })
+// enter and space open the modal if selected via tabbing
+gridContainer.addEventListener("keypress", (e) => {
+  if (e.code === "Enter" || e.code === "Space") {
+    const card = e.target.closest(".card")
+    const index = card.getAttribute("data-index")
+    displayModal(index)
+  }
+})
+
+//escape key closes modal
+document.onkeydown = (e) => {
+  console.log(e)
+  if (e.code === "Escape") {
+    overlay.classList.add("hidden")
+  }
+}
+
+// function so that cross becomes focus automatically
+modalClose.focus()
+
+//cross closes modal
+modalClose.addEventListener("click", () => {
+  overlay.classList.add("hidden")
+})
+
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    overlay.classList.add("hidden")
+  }
+})
